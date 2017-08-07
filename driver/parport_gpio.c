15abd99f7b1a7d5ff0787e7af453805c3f915f6a
Jim Garlick <garlick.jim@gmail.com>
2017-08-07 14:48:08 -0700

    driver: set polarity in dts not software
    Change the active low pins to active low in the device
    tree overlay, and then change the driver to stop inverting
    those pins in software.

--- driver/parport_gpio.c
+++ driver/parport_gpio.c
@@ -80,10 +80,10 @@ parport_gpio_read_control(struct parport *p)
 
 	spin_lock_irqsave (&ctx->lock, flags);
 
-	control =  (~gpiod_get_value (ctx->control->desc[0]) & 1) << 0;
-	control |= (~gpiod_get_value (ctx->control->desc[1]) & 1) << 1;
+	control =  gpiod_get_value (ctx->control->desc[0]);
+	control |= gpiod_get_value (ctx->control->desc[1]) << 1;
 	control |= gpiod_get_value (ctx->control->desc[2]) << 2;
-	control |= (~gpiod_get_value (ctx->control->desc[3]) & 1) << 3;
+	control |= gpiod_get_value (ctx->control->desc[3]) << 3;
 
 	spin_unlock_irqrestore (&ctx->lock, flags);
 
@@ -99,10 +99,10 @@ parport_gpio_write_control(struct parport *p, unsigned char control)
 
 	spin_lock_irqsave (&ctx->lock, flags);
 
-	value[0] = ~control & 1; // ~nStrobe
-	value[1] = (~control >> 1) & 1; // ~nAutoLF
+	value[0] = control & 1; // ~nStrobe
+	value[1] = (control >> 1) & 1; // ~nAutoLF
 	value[2] = (control >> 2) & 1; // nInitialize
-	value[3] = (~control >> 3) & 1; // ~nSelect
+	value[3] = (control >> 3) & 1; // ~nSelect
 	gpiod_set_array_value (ctx->control->ndescs, ctx->control->desc, value);
 
 	spin_unlock_irqrestore (&ctx->lock, flags);
@@ -118,16 +118,16 @@ parport_gpio_frob_control(struct parport *p, unsigned char mask,
 	spin_lock_irqsave (&ctx->lock, flags);
 
 	if ((mask & 1)) { // ~Strobe
-		gpiod_set_value(ctx->control->desc[0], ~(val & 1));
+		gpiod_set_value(ctx->control->desc[0], val & 1);
 	}
 	if ((mask & 2)) { // ~nAutoLF
-		gpiod_set_value(ctx->control->desc[1], ~((val >> 1) & 1));
+		gpiod_set_value(ctx->control->desc[1], (val >> 1) & 1);
 	}
 	if ((mask & 4)) { // nInitialize
 		gpiod_set_value(ctx->control->desc[2], (val >> 2) & 1);
 	}
 	if ((mask & 8)) { // ~nSelect
-		gpiod_set_value(ctx->control->desc[3], ~((val >> 3) & 1));
+		gpiod_set_value(ctx->control->desc[3], (val >> 3) & 1);
 	}
 
 	spin_unlock_irqrestore (&ctx->lock, flags);
@@ -144,11 +144,11 @@ parport_gpio_read_status(struct parport *p)
 
 	spin_lock_irqsave (&ctx->lock, flags);
 
-	status = (gpiod_get_value (ctx->status->desc[0]) << 3); // nError
-	status |= (gpiod_get_value (ctx->status->desc[1]) << 4); // Select
-	status |= (gpiod_get_value (ctx->status->desc[2]) << 5); // Paperout
-	status |= (gpiod_get_value (ctx->status->desc[3]) << 6); // nAck
-	status |= (~gpiod_get_value (ctx->status->desc[4]) & 1) << 7;//~Busy
+	status = gpiod_get_value (ctx->status->desc[0]) << 3; 	// nError
+	status |= gpiod_get_value (ctx->status->desc[1]) << 4; 	// Select
+	status |= gpiod_get_value (ctx->status->desc[2]) << 5; 	// Paperout
+	status |= gpiod_get_value (ctx->status->desc[3]) << 6; 	// nAck
+	status |= gpiod_get_value (ctx->status->desc[4]) << 7;	//~Busy
 
 	spin_unlock_irqrestore (&ctx->lock, flags);
 
